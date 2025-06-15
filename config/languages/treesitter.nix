c8288129e78c87d913ba7c821a35b82f115af45d
Laszlo Bacsi <lackac@lackac.hu>
2025-06-15 23:18:50 +0200

    feat(languages): treesitter grammars for occasional languages

--- config/languages/treesitter.nix
+++ config/languages/treesitter.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   vim.treesitter = {
     enable = true;
 
@@ -20,5 +20,55 @@
       init = null;
       incrementByScope = null;
     };
+
+    grammars = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+      # these are languages I might need to browse occasionally but don't need more support than a grammar
+      asm
+      beancount
+      c
+      c-sharp
+      clojure
+      cpp
+      csv
+      diff
+      dockerfile
+      editorconfig
+      elm
+      fish
+      git_config
+      gitcommit
+      gitignore
+      gleam
+      go
+      http
+      java
+      jinja
+      jinja_inline
+      jq
+      json
+      julia
+      just
+      kotlin
+      latex
+      ledger
+      liquid
+      make
+      mermaid
+      nginx
+      objc
+      ocaml
+      perl
+      php
+      r
+      rust
+      scala
+      sql
+      svelte
+      swift
+      toml
+      vue
+      xml
+      zig
+    ];
   };
 }
