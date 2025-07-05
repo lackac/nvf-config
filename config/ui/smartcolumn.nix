bdb70fae02817efef6e9d66b62a134cd921950e4
Laszlo Bacsi <lackac@lackac.hu>
2025-07-05 23:12:52 +0200

    fix: don't show smartcolumn on dashboard and other snacks buffers

--- config/ui/smartcolumn.nix
+++ config/ui/smartcolumn.nix
@@ -3,6 +3,15 @@
     enable = true;
     setupOpts = {
       colorcolumn = ["80" "100" "120"];
+      disabled_filetypes = [
+        "help"
+        "text"
+        "markdown"
+        "snacks_dashboard"
+        "snacks_picker_input"
+        "snacks_picker_preview"
+        "snacks_picker_list"
+      ];
     };
   };
 }
