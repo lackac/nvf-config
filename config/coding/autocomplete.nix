d4298f36099339f5650b0bf66b5f2bd82fa81991
Laszlo Bacsi <lackac@lackac.hu>
2025-07-08 20:15:42 +0200

    feat(autocomplete): only show spelling suggestions where it makes sense

--- config/coding/autocomplete.nix
+++ config/coding/autocomplete.nix
@@ -1,4 +1,6 @@
-{
+{lib, ...}: let
+  inherit (lib.generators) mkLuaInline;
+in {
   vim.autocomplete.blink-cmp = {
     enable = true;
     friendly-snippets.enable = true;
@@ -15,7 +17,6 @@
 
     sourcePlugins = {
       emoji.enable = true;
-      ripgrep.enable = true;
       spell.enable = true;
     };
 
@@ -24,24 +25,43 @@
         preset = "enter";
 
         "<C-space>" = [];
+        "<C-s>" = ["show" "show_documentation" "hide_documentation"];
         "<C-y>" = ["select_and_accept"];
+        "<CR>" = ["accept" "fallback"];
 
         "<C-d>" = ["scroll_documentation_down" "fallback"];
         "<C-u>" = ["scroll_documentation_up" "fallback"];
-        "<esc>" = ["hide" "fallback"];
       };
 
       cmdline.sources = [];
 
       sources = {
-        default = [
-          "snippets"
-          "lsp"
-          "path"
-          "ripgrep"
-          "buffer"
-          "emoji"
-        ];
+        providers = {
+          spell = {
+            opts = {
+              preselect_current_word = false;
+              enable_in_context = mkLuaInline ''
+                function()
+                  local curpos = vim.api.nvim_win_get_cursor(0)
+                  local captures = vim.treesitter.get_captures_at_pos(
+                    0,
+                    curpos[1] - 1,
+                    curpos[2] - 1
+                  )
+                  local in_spell_capture = false
+                  for _, cap in ipairs(captures) do
+                    if cap.capture == 'spell' then
+                      in_spell_capture = true
+                    elseif cap.capture == 'nospell' then
+                      return false
+                    end
+                  end
+                  return in_spell_capture
+                end
+              '';
+            };
+          };
+        };
       };
 
       snippets.preset = "luasnip";
@@ -54,10 +74,11 @@
       completion = {
         list = {
           selection = {
-            preselect = false;
+            preselect = true;
             auto_insert = true;
           };
         };
+        trigger.show_in_snippet = false;
 
         documentation.window.border = "rounded";
         menu.border = "rounded";
