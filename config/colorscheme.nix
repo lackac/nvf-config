4f20504314fcf4519aef3ea30d50b8e0b13db4b4
Laszlo Bacsi <lackac@lackac.hu>
2026-03-03 20:02:58 +0100

    feat: remove unnecessary background switching now that Neovim handles Mode 2031

--- config/colorscheme.nix
+++ config/colorscheme.nix
@@ -3,12 +3,5 @@
     enable = true;
     name = "solarized";
     style = "solarized-winter";
-    extraConfig = ''
-      if vim.fn.system("defaults read -g AppleInterfaceStyle") == "Dark\n" then
-        vim.opt.background = "dark"
-      else
-        vim.opt.background = "light"
-      end
-    '';
   };
 }
