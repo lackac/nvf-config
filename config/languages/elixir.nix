be9cf935fdcaf3aa54af43654b6a5e04b09dc5ce
Laszlo Bacsi <lackac@lackac.hu>
2026-07-10 16:26:20 +0200

    chore: bump inputs and fix warning

--- config/languages/elixir.nix
+++ config/languages/elixir.nix
@@ -1,8 +1,21 @@
 {
+  pkgs,
+  ...
+}:
+{
   vim.languages.elixir = {
     enable = true;
     elixir-tools.enable = false;
+    format.enable = false;
     lsp.enable = false;
+  };
+
+  vim.formatter.conform-nvim = {
+    enable = true;
+    setupOpts = {
+      formatters.mix.command = "${pkgs.beamPackages.elixir}/bin/mix";
+      formatters_by_ft.elixir = [ "mix" ];
+    };
   };
 
   vim.lsp.servers.expert = {
