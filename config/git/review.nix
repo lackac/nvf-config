fdc6e88738c6e6cd14e73bc826e3582e8dfde496
Laszlo Bacsi <lackac@lackac.hu>
2026-09-10 12:09:46 +0200

    style(review): make colordiff highlights work better in solarized themes

--- config/git/review.nix
+++ config/git/review.nix
@@ -9,8 +9,38 @@
         cmd = [ "CodeDiff" ];
         beforeSetup = ''
           vim.env.CODEDIFF_WATCHER_PATH = "${pkgs.codediff-watcher}/bin/codediff-watcher"
+
+          local function set_codediff_highlights()
+            local light = vim.o.background == "light"
+            local palette = light
+              and require("solarized.palette.solarized-light").solarized
+              or require("solarized.palette").solarized
+
+            vim.api.nvim_set_hl(0, "CodeDiffAdaptiveInsert", { bg = light and palette.mix_green or "#203f20" })
+            vim.api.nvim_set_hl(0, "CodeDiffAdaptiveDelete", { bg = light and palette.mix_red or "#38262c" })
+
+            if package.loaded["codediff.ui.highlights"] then
+              require("codediff.ui.highlights").setup()
+            end
+          end
+
+          set_codediff_highlights()
+
+          vim.api.nvim_create_autocmd("ColorScheme", {
+            group = vim.api.nvim_create_augroup("nvf_codediff_highlights", { clear = true }),
+            callback = set_codediff_highlights,
+          })
+          vim.api.nvim_create_autocmd("OptionSet", {
+            group = "nvf_codediff_highlights",
+            pattern = "background",
+            callback = set_codediff_highlights,
+          })
         '';
         setupModule = "codediff";
+        setupOpts.highlights = {
+          line_insert = "CodeDiffAdaptiveInsert";
+          line_delete = "CodeDiffAdaptiveDelete";
+        };
       };
 
       "review.nvim" = {
